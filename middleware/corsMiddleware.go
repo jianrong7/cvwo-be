102fadc6b6df9a60aa154dcbf1b2d06c9d6c7522
jianrong7_ <jianrong@99.co>
2022-12-23 17:12:36 +0800

    limit origins

--- middleware/corsMiddleware.go
+++ middleware/corsMiddleware.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
-			c.Header("Access-Control-Allow-Origin", "*")
+			c.Header("Access-Control-Allow-Origin", "https://cvwo-fe.s3.ap-southeast-1.amazonaws.com")
 			c.Header("Access-Control-Allow-Credentials", "true")
 			c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
 			c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, OPTIONS, PATCH")
